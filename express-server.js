0fbb664923abc31f4373cf4ce2ced93c8e657c9f
Amy Martin <amymariemart@gmail.com>
2022-04-18 21:10:48 -0700

    Implemented generateRandomString function

--- express-server.js
+++ express-server.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 8080; 
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -17,6 +28,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls-index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls-new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
