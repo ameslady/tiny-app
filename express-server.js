8a77b48d5a17d56f3c020b6e5f4dda8c41089e9c
Amy Martin <amymariemart@gmail.com>
2022-04-18 20:18:55 -0700

    Created new route to urls-index

--- express-server.js
+++ express-server.js
@@ -1,36 +1,46 @@
 const express = require("express");
 const app = express();
-const port = 8080; // default port 8080
+const port = 8080; 
 
 app.set("view engine", "ejs");
 
-const urlDatase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xk": "http://www.google.com"
+  "9sm5xK": "http://www.google.com"
 };
 
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls-index", templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls-show", templateVars);
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening to port ${port}!`);
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+//  });
+ 
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+//  });
+
+ app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
