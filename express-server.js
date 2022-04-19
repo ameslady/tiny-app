bf1db3fbdd638674070e6f352d600b0d19c2c610
Amy Martin <amymariemart@gmail.com>
2022-04-18 21:32:34 -0700

    Saved new URLS to the url database, and implemented a redirect to the URL

--- express-server.js
+++ express-server.js
@@ -32,11 +32,16 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body.longURL;
   res.send("Ok"); // Respond with 'Ok' (we will replace this);
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls-new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
