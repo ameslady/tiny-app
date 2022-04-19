b9ed877931b7dd7cb31f375e40dc1221a4029406
Amy Martin <amymariemart@gmail.com>
2022-04-18 19:28:10 -0700

    Installed ejs in project

--- express-server.js
+++ express-server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const port = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
